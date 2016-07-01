5f5a0c9cf6640ba682f239e784cc470244452575
Greg <stysiok@o2.pl>
2016-07-01 16:30:26 +0200

    Start
    This app will check if the files in given directory were modyfied. Then
    it will print all of them, and you can mark witch of them you wanto to
    upload to your google drive.

---
+++ GoogleDiskApp/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace GoogleDiskApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zakończToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.opcjeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edytujŚcieżkęToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.daneUżytkownikaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aktualizujPlikŹródłowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pomocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wyświetlPomocToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.autorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programToolStripMenuItem,
+            this.opcjeToolStripMenuItem,
+            this.pomocToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(616, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // programToolStripMenuItem
+            // 
+            this.programToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.zakończToolStripMenuItem});
+            this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+            this.programToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.programToolStripMenuItem.Text = "Program";
+            // 
+            // zakończToolStripMenuItem
+            // 
+            this.zakończToolStripMenuItem.Name = "zakończToolStripMenuItem";
+            this.zakończToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.zakończToolStripMenuItem.Text = "Zakończ";
+            // 
+            // opcjeToolStripMenuItem
+            // 
+            this.opcjeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.edytujŚcieżkęToolStripMenuItem,
+            this.daneUżytkownikaToolStripMenuItem,
+            this.aktualizujPlikŹródłowyToolStripMenuItem});
+            this.opcjeToolStripMenuItem.Name = "opcjeToolStripMenuItem";
+            this.opcjeToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.opcjeToolStripMenuItem.Text = "Opcje";
+            // 
+            // edytujŚcieżkęToolStripMenuItem
+            // 
+            this.edytujŚcieżkęToolStripMenuItem.Name = "edytujŚcieżkęToolStripMenuItem";
+            this.edytujŚcieżkęToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.edytujŚcieżkęToolStripMenuItem.Text = "Edytuj ścieżkę";
+            // 
+            // daneUżytkownikaToolStripMenuItem
+            // 
+            this.daneUżytkownikaToolStripMenuItem.Name = "daneUżytkownikaToolStripMenuItem";
+            this.daneUżytkownikaToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.daneUżytkownikaToolStripMenuItem.Text = "Dane użytkownika";
+            // 
+            // aktualizujPlikŹródłowyToolStripMenuItem
+            // 
+            this.aktualizujPlikŹródłowyToolStripMenuItem.Name = "aktualizujPlikŹródłowyToolStripMenuItem";
+            this.aktualizujPlikŹródłowyToolStripMenuItem.Size = new System.Drawing.Size(199, 22);
+            this.aktualizujPlikŹródłowyToolStripMenuItem.Text = "Aktualizuj plik źródłowy";
+            // 
+            // pomocToolStripMenuItem
+            // 
+            this.pomocToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wyświetlPomocToolStripMenuItem,
+            this.autorToolStripMenuItem});
+            this.pomocToolStripMenuItem.Name = "pomocToolStripMenuItem";
+            this.pomocToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.pomocToolStripMenuItem.Text = "Pomoc";
+            // 
+            // wyświetlPomocToolStripMenuItem
+            // 
+            this.wyświetlPomocToolStripMenuItem.Name = "wyświetlPomocToolStripMenuItem";
+            this.wyświetlPomocToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.wyświetlPomocToolStripMenuItem.Text = "Wyświetl Pomoc";
+            // 
+            // autorToolStripMenuItem
+            // 
+            this.autorToolStripMenuItem.Name = "autorToolStripMenuItem";
+            this.autorToolStripMenuItem.Size = new System.Drawing.Size(162, 22);
+            this.autorToolStripMenuItem.Text = "Autor";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 417);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zakończToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem opcjeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pomocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem edytujŚcieżkęToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem daneUżytkownikaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aktualizujPlikŹródłowyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem wyświetlPomocToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem autorToolStripMenuItem;
+    }
+}
+
